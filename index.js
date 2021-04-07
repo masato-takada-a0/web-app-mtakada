4d193b9844f5c58a3e793dafac71a617d96efc2e
masato-takada-a0 <80874255+masato-takada-a0@users.noreply.github.com>
2021-04-07 09:31:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,12 @@ app.use(
    secret: SESSION_SECRET,
    auth0Logout: true,
    baseURL: APP_URL,
+      // 👇 add this 👇
+   authorizationParams: {
+     response_type: "code id_token",
+     audience: "https://expenses-api",
+   },
+   // 👆 add this 👆
  })
 );
 
@@ -67,7 +73,15 @@ app.get("/user", async (req, res) => {
 
  app.get("/expenses", requiresAuth(), async (req, res, next) => {
    try {
-     const expenses = await axios.get(`${API_URL}/reports`);
+        // 👇 get the token from the request 👇
+      const { token_type, access_token } = req.oidc.accessToken;
+   // 👇 then send it as an authorization header 👇
+      const expenses = await axios.get(`${API_URL}/reports`, {
+      headers: {
+       Authorization: `${token_type} ${access_token}`,
+     },
+   });
+//     const expenses = await axios.get(`${API_URL}/reports`);
      res.render("expenses", {
        user: req.oidc && req.oidc.user,
        expenses: expenses.data,
